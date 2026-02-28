bd4ca61179719b2f386624bd2e2ab51427f2475d
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2026-02-28 23:47:16 +0600

    updated robots xtxt also

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const BASE_URL = "https://www.xiomtech.net/";
+const BASE_URL = "https://www.xiomtech.net";
 
 export default function robots(): MetadataRoute.Robots {
   return {
